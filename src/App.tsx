1725f23bb1b5ef63fa695d46117d2e036460996d
Randy Chung <randy.chung@gmail.com>
2020-10-16 16:19:28 -0400

    Added imports for bootstrap styles into the main app and storybook.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // bootstrap styles
 
 import Button from 'react-bootstrap/Button';
 
